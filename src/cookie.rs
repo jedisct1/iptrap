598a5a16c0467584257dc0fe5b80cc5772899776
Frank Denis <github@pureftpd.org>
2014-02-23 10:47:23 -0800

    Simplify the code computing cookies

--- src/cookie.rs
+++ src/cookie.rs
@@ -1,19 +1,30 @@
 
-use std::hash::sip::SipState;
+use std::hash::sip;
+use std::hash::Hash;
 
 pub struct SipHashKey {
     k1: u64,
     k2: u64
 }
 
+#[deriving(Hash)]
+struct CookieInput<'s> {
+    ip_src: &'s [u8],
+    ip_dst: &'s [u8],
+    th_sport: u16,
+    th_dport: u16,
+    uts: u64
+}
+
 #[allow(unused_must_use)]
 pub fn tcp(ip_src: &[u8], ip_dst: &[u8], th_sport: u16, th_dport: u16,
            sk: SipHashKey, uts: u64) -> u32 {
-    let mut hash = SipState::new(sk.k1, sk.k2);
-    hash.write(ip_src);
-    hash.write(ip_dst);
-    hash.write_le_u16(th_sport);
-    hash.write_le_u16(th_dport);
-    hash.write_le_u64(uts);
-    hash.result() as u32
+    let input = CookieInput {
+        ip_src: ip_src,
+        ip_dst: ip_dst,
+        th_sport: th_sport,
+        th_dport: th_dport,
+        uts: uts
+    };
+    sip::hash_with_keys(sk.k1, sk.k2, &input) as u32
 }
