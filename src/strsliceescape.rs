729d46ef4d1cdcf7434939b378dd6aa7cb002fc8
Frank Denis <github@pureftpd.org>
2014-03-01 14:49:24 -0800

    Just output '?' for non-ASCII, non-printable chars.

--- src/strsliceescape.rs
+++ src/strsliceescape.rs
@@ -1,5 +1,4 @@
 
-use std::char;
 use std::str;
 
 pub trait StrSliceEscape<'a> {
@@ -10,14 +9,10 @@ impl<'a> StrSliceEscape<'a> for &'a str {
     fn escape_default_except_lf(&self) -> ~str {
         let mut out = str::with_capacity(self.len());
         for c in self.chars() {
-            match c {
-                '\r' => { out.push_char('\\'); out.push_char('r'); }
-                '\\' => { out.push_char('\\'); out.push_char('\\'); }
-                '\'' => { out.push_char('\\'); out.push_char('\''); }
-                '"'  => { out.push_char('\\'); out.push_char('"'); }
-                '\n' | '\t' | '\x20' .. '\x7e' => { out.push_char(c); }
-                _ => char::escape_unicode(c, |c| { out.push_char(c); })
-            }
+            out.push_char(match c {
+                '\r' | '\n' | '\t' | '\x20' .. '\x7e' => c,
+                _ => '?'
+            })
         }
         out
     }
