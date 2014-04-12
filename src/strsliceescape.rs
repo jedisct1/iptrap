40fc751842e923acf505127bee4a944bee246d69
Frank Denis <github@pureftpd.org>
2014-04-11 23:34:13 -0700

    Str::with_capacity() is gone

--- src/strsliceescape.rs
+++ src/strsliceescape.rs
@@ -1,19 +1,17 @@
 
-use std::str;
-
 pub trait StrSliceEscape<'a> {
     fn escape_default_except_lf(&self) -> ~str;
 }
 
 impl<'a> StrSliceEscape<'a> for &'a str {
     fn escape_default_except_lf(&self) -> ~str {
-        let mut out = str::with_capacity(self.len());
+        let mut out = StrBuf::with_capacity(self.len());
         for c in self.chars() {
             out.push_char(match c {
                 '\r' | '\n' | '\t' | '\x20' .. '\x7e' => c,
                 _ => '?'
             })
         }
-        out
+        out.into_owned()
     }
 }
