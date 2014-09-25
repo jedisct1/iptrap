de84f5a79b23aebf20bcece416c740209b006811
Frank Denis <github@pureftpd.org>
2014-09-25 00:31:18 -0700

    push_char -> push

--- src/strsliceescape.rs
+++ src/strsliceescape.rs
@@ -7,7 +7,7 @@ impl StrSliceEscape for String {
     fn escape_default_except_lf(&self) -> String {
         let mut out = String::with_capacity(self.len());
         for c in self.as_slice().chars() {
-            out.push_char(match c {
+            out.push(match c {
                 '\r' | '\n' | '\t' | '\x20' .. '\x7e' => c,
                 _ => '?'
             })
